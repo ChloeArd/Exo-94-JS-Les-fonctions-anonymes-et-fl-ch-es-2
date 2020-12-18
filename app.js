984261efdb3736abb13fe1d8fe466300ad3ce5ee
ChloeArd <chloe.ardoise@gmail.com>
2020-12-18 16:14:03 +0100

    Exercice 94

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+let tableauTrois = [tableauUn.map(item =>{
+    return item ** item;
+})];
+console.log(tableauTrois);
+
+let tableauQuatre = [tableauUn.map(item =>{
+    return item * 10;
+})];
+console.log(tableauQuatre);
+
+let tableauCinq = [tableauUn.map(item =>{
+    return (item + 2) / 18;
+})];
+console.log(tableauCinq);
+
