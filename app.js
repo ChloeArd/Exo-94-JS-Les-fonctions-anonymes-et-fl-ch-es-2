6a8572d06040a9c552914b5e3c724e741ad272d2
ChloeArd <chloe.ardoise@gmail.com>
2020-12-19 17:25:31 +0100

    Exercice 94

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+//TableauUn
 let tableauTrois = [tableauUn.map(item =>{
     return item ** item;
 })];
@@ -26,3 +27,18 @@ console.log("item % 2 => " + tableauSept);
 let tableauHuit = tableauUn.filter(item => (item * 3) > 10);
 console.log("(item * 3) > 10 => " + tableauHuit);
 
+//TableauDeux
+let tableauNeuf = [tableauDeux.map(item =>{
+    return item.length;
+})];
+console.log("item.length => " + tableauNeuf);
+
+let tableauDix = [tableauDeux.map(item =>{
+    return "L'élément " + item + " à une longeur de " + item.length;
+})];
+console.log("item + item.length => " + tableauDix);
+
+let tableauOnze = [tableauDeux.map(item =>{
+    return item + item;
+})];
+console.log("item + item => " + tableauOnze);
