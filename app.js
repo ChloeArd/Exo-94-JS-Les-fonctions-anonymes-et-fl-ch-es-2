baa179543f276977e3d2927cecb3e75739c705b1
ChloeArd <chloe.ardoise@gmail.com>
2020-12-19 17:17:39 +0100

    Exercice 94

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidul
 let tableauTrois = [tableauUn.map(item =>{
     return item ** item;
 })];
-console.log(tableauTrois);
+console.log("item ** item => " + tableauTrois);
 
 let tableauQuatre = [tableauUn.map(item =>{
     return item * 10;
 })];
-console.log(tableauQuatre);
+console.log("item * 100 => " + tableauQuatre);
 
 let tableauCinq = [tableauUn.map(item =>{
     return (item + 2) / 18;
 })];
-console.log(tableauCinq);
+console.log("item + 2 / 18 => " + tableauCinq);
+
+let tableauSix = tableauUn.filter(item => item > 2);
+console.log("item > 2 => " + tableauSix);
+
+let tableauSept = tableauUn.filter(item => item % 2);
+console.log("item % 2 => " + tableauSept);
+
+let tableauHuit = tableauUn.filter(item => (item * 3) > 10);
+console.log("(item * 3) > 10 => " + tableauHuit);
 
