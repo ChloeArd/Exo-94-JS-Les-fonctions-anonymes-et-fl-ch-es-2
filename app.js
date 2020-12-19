9ff30559ff35ccb85d12282d68a1aaef264ca028
ChloeArd <chloe.ardoise@gmail.com>
2020-12-19 17:54:14 +0100

    Exercice 94

--- app.js
+++ app.js
@@ -2,52 +2,45 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+let creerDiv = (text) => {
+    let divHTML = document.createElement("div");
+    divHTML.innerHTML = text;
+    document.body.appendChild(divHTML)
+}
 //TableauUn
-let tableauTrois = [tableauUn.map(item =>{
-    return item ** item;
-})];
-console.log("item ** item => " + tableauTrois);
+let tableauTrois = tableauUn.map(item => item ** item);
+creerDiv("TableauUn: item ** item => " + tableauTrois);
 
-let tableauQuatre = [tableauUn.map(item =>{
-    return item * 10;
-})];
-console.log("item * 100 => " + tableauQuatre);
+let tableauQuatre = tableauUn.map(item => item * 10);
+creerDiv("TableauUn: item * 100 => " + tableauQuatre);
 
-let tableauCinq = [tableauUn.map(item =>{
-    return (item + 2) / 18;
-})];
-console.log("item + 2 / 18 => " + tableauCinq);
+let tableauCinq = tableauUn.map(item => (item + 2) / 18);
+creerDiv("TableauUn: item * 100 => " + tableauQuatre);
 
 let tableauSix = tableauUn.filter(item => item > 2);
-console.log("item > 2 => " + tableauSix);
+creerDiv("TableauUn: item > 2 => " + tableauSix);
 
 let tableauSept = tableauUn.filter(item => item % 2);
-console.log("item % 2 => " + tableauSept);
+creerDiv("TableuUn: item % 2 => " + tableauSept);
 
 let tableauHuit = tableauUn.filter(item => (item * 3) > 10);
-console.log("(item * 3) > 10 => " + tableauHuit);
+creerDiv("TableauUn: (item * 3) > 10 => " + tableauHuit);
 
 //TableauDeux
-let tableauNeuf = [tableauDeux.map(item =>{
-    return item.length;
-})];
-console.log("item.length => " + tableauNeuf);
+let tableauNeuf = tableauDeux.map(item => item.length);
+creerDiv("TableauDeux: item.length => " + tableauNeuf);
 
-let tableauDix = [tableauDeux.map(item =>{
-    return "L'élément " + item + " à une longeur de " + item.length;
-})];
-console.log("item + item.length => " + tableauDix);
+let tableauDix = tableauDeux.map(item => "L'élément " + item + " à une longeur de " + item.length);
+creerDiv("TableauDeux: item + item.length => " + tableauDix);
 
-let tableauOnze = [tableauDeux.map(item =>{
-    return item + item;
-})];
-console.log("item + item => " + tableauOnze);
+let tableauOnze = tableauDeux.map(item => item + item );
+creerDiv("TableauDeux: item + item => " + tableauOnze);
 
 let tableauDouze = tableauDeux.filter(item => item.length > (2 + 2));
-console.log("item.length > (2 + 2) => " + tableauDouze);
+creerDiv("TableauDeux: item.length > (2 + 2) => " + tableauDouze);
 
 let tableauTreize = tableauDeux.filter(item => item.length % 2);
-console.log("item.length % 2 => " + tableauTreize);
+creerDiv("TableauDeux: item.length % 2 => " + tableauTreize);
 
 let tableauQuatorze = tableauDeux.filter(item => (item.length - 3) > 10);
-console.log("(item.length - 3) > 10 => " + tableauQuatorze);
+creerDiv("TableauDeux: (item.length - 3) > 10 => " + tableauQuatorze);
